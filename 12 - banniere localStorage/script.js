7f54f521d40e505a5b2435d10728b5057604c734
Fraxoo <fraxo.officiel@gmail.com>
2025-09-24 14:52:20 +0200

    exo 12 fini

--- 12 - banniere localStorage/script.js
+++ 12 - banniere localStorage/script.js
@@ -4,3 +4,15 @@
  * getItem()
  * Fonctionnement de Stockage et des cookies
  */
+const acceptButton_elem = document.querySelector(".btn-accept");
+const cookies_elem = document.querySelector(".cookies")
+const okCookie = localStorage.getItem("cookie");
+
+if(okCookie === "1"){
+    cookies_elem.style.display = "none"
+};
+
+acceptButton_elem.onclick = function(){
+    cookies_elem.classList.add("fade");
+    localStorage.setItem("cookie","1");
+}
