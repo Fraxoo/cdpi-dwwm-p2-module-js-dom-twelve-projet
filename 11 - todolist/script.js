221c880ecfc4c9c32ea61cdf423058225310056a
Fraxoo <fraxo.officiel@gmail.com>
2025-09-24 16:33:21 +0200

    todolist

--- 11 - todolist/script.js
+++ 11 - todolist/script.js
@@ -1,20 +1,39 @@
-/**
- * element.parentElement
- * element.remove()
- */
-
-// 1. Je recupere le forumlaire de saisi d'un nouvelle tache
+const listTasksDiv_elem = document.querySelector(".list-tasks");
 const form = document.querySelector("#form");
-// 2. Je recupere le conteneur des taches
-const listTasks = document.querySelector(".list-tasks");
 
-// 3. Lorsque le formulaire est soumis
-form.addEventListener("submit",function(e){
-    
-    // ...
-    
-    
-    // Je reset le contenu du formulaire pour eviter que le texte reste une fois la tache crée (ergonomique)
-    form.reset();
-});
 
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    const formData = new FormData(form);
+    const task = document.createElement("div");
+    task.classList.add("task");
+    listTasksDiv_elem.appendChild(task);
+
+    const taskP = document.createElement("p");
+    taskP.textContent = formData.get("tache");
+    task.appendChild(taskP);
+    console.log(taskP);
+    
+
+    const taskButtonDele = document.createElement("btn");
+    taskButtonDele.classList.add("btn-delete");
+    task.appendChild(taskButtonDele);
+
+    const logoButtonDel = document.createElement("i");
+    logoButtonDel.classList.add("fas");
+    logoButtonDel.classList.add("fa-trash-alt");
+    taskButtonDele.appendChild(logoButtonDel);
+
+    const taskButtonArch = document.createElement("btn");
+    taskButtonArch.classList.add("btn-archive");
+    task.appendChild(taskButtonArch);
+
+    const logoButtonArch = document.createElement("i");
+    logoButtonArch.classList.add("fas");
+    logoButtonArch.classList.add("fa-check-circle");
+    taskButtonArch.appendChild(logoButtonArch);
+
+
+
+})
