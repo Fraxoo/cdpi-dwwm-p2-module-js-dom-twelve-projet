6cddcac7b540692666d883359a5f3b821e83ebc2
Fraxoo <fraxo.officiel@gmail.com>
2025-09-24 11:59:20 +0200

    exo 6 fini

--- 6 - toast/script.js
+++ 6 - toast/script.js
@@ -11,3 +11,20 @@
 const btn = document.querySelector("button");
 // 2. Je recupere le conteneur de toasts
 const ctnToasts = document.querySelector(".container-toasts");
+
+btn.onclick = function () {
+    const saveFileDiv_elem = document.createElement("div");
+    saveFileDiv_elem.classList.add("toast");
+    const saveFileP_elem = document.createElement("p");
+    saveFileP_elem.innerText = "Votre fichier est enregister !";
+    ctnToasts.appendChild(saveFileDiv_elem);
+    saveFileDiv_elem.appendChild(saveFileP_elem);
+    setTimeout(() => {
+        saveFileDiv_elem.classList.add("fade");
+    }, 1000);
+    setTimeout(() => {
+        saveFileDiv_elem.remove();
+        saveFileP_elem.remove();
+        ctnToasts.classList.remove("fade");
+    }, 2000)
+}
